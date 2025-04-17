a35a154908c452dba0ecb5e9ac63e5ceb0442a88
Ralf S. Engelschall <rse@engelschall.com>
2025-04-17 14:57:55 +0200

    add missing header

--- rundown-lib/src/lib.d.ts
+++ rundown-lib/src/lib.d.ts
@@ -1,3 +1,8 @@
+/*
+**  Rundown - Render Rundown Scripts for Teleprompting
+**  Copyright (c) 2023-2025 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
+*/
 
 declare global {
     /* eslint no-unused-vars: off */
