f89b286227c171984f74ce2392de7dc0c8935a91
Ralf S. Engelschall <rse@engelschall.com>
2025-09-20 17:44:08 +0200

    correctly exit with non-zero exit code in case of error

--- rundown-7-dst/etc/docker-entrypoint.bash
+++ rundown-7-dst/etc/docker-entrypoint.bash
@@ -8,5 +8,6 @@ elif [[ $1 = "rundown-bridge" ]]; then
     exec node --no-warnings rundown-bridge.js ${1+"$@"}
 else
     echo "ERROR: invalid command (expected \"rundown-cli\" or \"rundown-bridge\")" 1>&2
+    exit 1
 fi
 
