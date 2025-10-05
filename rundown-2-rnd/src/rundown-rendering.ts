9bdbdb06751f53932188c771993751946fc75583
Ralf S. Engelschall <rse@engelschall.com>
2025-10-05 12:35:40 +0200

    fix regexp

--- rundown-2-rnd/src/rundown-rendering.ts
+++ rundown-2-rnd/src/rundown-rendering.ts
@@ -132,7 +132,7 @@ export class RundownRendering {
                 const bb = state.getBoundingClientRect()
                 const text = (state as HTMLSpanElement).innerText
                 const kv: { [ key: string ]: string | number | boolean } = {}
-                const matches = text.matchAll(/\b([a-zA-Z][a-zA-Z0-9:-]*)(?:=(?:"([^"]*)"|(\\S+)))?\b/g)
+                const matches = text.matchAll(/\b([a-zA-Z][a-zA-Z0-9:-]*)(?:=(?:"([^"]*)"|(\S+)))?\b/g)
                 for (const match of matches) {
                     const key = match[1]
                     const val = match[2] ?? match[3] ?? undefined
